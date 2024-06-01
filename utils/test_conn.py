1e2f2bf31111c1175b7fc2cdc91e5e2fe0c90ec8
Afonso Diniz <afonso@rauva.com>
2024-06-01 19:39:54 +0100

    change to Rauva TEch

--- utils/test_conn.py
+++ utils/test_conn.py
@@ -1,10 +1,10 @@
 import http.client
 
-conn = http.client.HTTPSConnection("rauvaservicesunip.app.invoicexpress.com")
+conn = http.client.HTTPSConnection("rauvaportugalunip.app.invoicexpress.com")
 
 headers = { 'accept': "application/json" }
 
-conn.request("GET", "/invoices.json?api_key=5a79167291dadd03d78781091f675786952c4e79&page=3", headers=headers)
+conn.request("GET", "/invoices.json?api_key=88ed9dc4797876277ed806ef178ed073dd3a565d&page=3", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
